01a8f8bce3b278ebda05080bdbf7b30a257aff0d
Qun He <hawkbee@gmail.com>
2016-03-26 21:30:16 +0800

    Finish tutorial 4.
    Use generic views.

--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, Http404, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.template import loader
+from django.views import generic
 from .models import Question, Choice
 
 def index(request):
@@ -38,3 +39,19 @@ def vote(request, question_id):
         choice.votes += 1
         choice.save()
     return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
